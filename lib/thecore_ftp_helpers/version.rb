262ab7ed96d339bfc9aa7b720f3479c956834306
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-03-16 11:34:32 +0000

    Add Chomp to version

--- lib/thecore_ftp_helpers/version.rb
+++ lib/thecore_ftp_helpers/version.rb
@@ -1,3 +1,3 @@
 module ThecoreFtpHelpers
-  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`}"
+  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
 end
